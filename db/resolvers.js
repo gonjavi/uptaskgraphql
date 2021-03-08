f8a93cd9500d624df62e90e0f337dd0595011e14
manci <gmancillapaz@gmail.com>
2021-03-08 11:04:27 -0500

    instalar mongoose y dotenv, conectar a base de datos atlas

---
+++ db/resolvers.js
@@ -0,0 +1,30 @@
+
+const cursos = [
+  {
+    titulo: 'JavaScript moderno guia definitiva construye 10 proyectos',
+    tecnologia: 'Javascript ES6',
+  },
+  {
+    titulo: 'React - La guiía complet: hooks Context redux MERN +15 APPs',
+    tecnologia: 'React',
+  },
+  {
+    titulo: 'Node.js - Bootcamp desarrollo web inc. MVC y REST APIs',
+    tecnologia: 'Node.js',
+  },
+  {
+    titulo: 'ReactJS Avanzado - Fullstack React GraphQL y APollo',
+    tecnologia: 'React',
+  }
+];
+
+
+const resolvers = {
+  Query: {
+    obtenerCursos: () => cursos,
+
+    obtenerTecnologia: () => cursos
+  }
+}
+
+module.exports = resolvers;
